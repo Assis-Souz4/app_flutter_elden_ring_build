9449a68517fbda9f5452771382811fbf2fe73827
Assis Souza <assissouza-dev@hotmail.com>
2024-09-19 20:07:46 -0300

    BuildCreate

---
+++ lib/components/build.dart
@@ -0,0 +1,83 @@
+import 'package:app_flutter_elden_ring_build/components/colors_app.dart';
+import 'package:flutter/material.dart';
+
+class ShowBuild extends StatefulWidget {
+  const ShowBuild({required this.nameBuild, super.key});
+  //
+  final String nameBuild;
+  // final String imageBuild;
+  //
+  @override
+  State<ShowBuild> createState() => _CreateBuildState();
+}
+
+class _CreateBuildState extends State<ShowBuild> {
+  //
+  int levelBuild = 150;
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding:
+              const EdgeInsets.only(top: 0, left: 20, right: 20, bottom: 10),
+          child: Container(
+            width: double.infinity,
+            // color: Colors.grey[350],
+            height: 140,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 160,
+                    height: 140,
+                    child: const Text('image'),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Name Build',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Level Build',
+                            style: TextStyle(
+                              color: AppColors.fontsSecundary,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Edit'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_flutter_elden_ring_build/components/app_bar_decoration.dart';
+import 'package:app_flutter_elden_ring_build/components/build.dart';
 import 'package:app_flutter_elden_ring_build/components/seach_build.dart';
 import 'package:flutter/material.dart';
 
@@ -14,17 +15,49 @@ class _ShowBuildsPageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getAppBar('Build Manager'),
-      body: ListView(
+      body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: SizedBox(
-              height: 50,
-              child: getSeachBuild(),
-            ),
+          Container(
+            height: 50,
           ),
-          
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 0, left: 10, right: 10, bottom: 0),
+            child: getSeachBuild(),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                ShowBuild(nameBuild: ''),
+                SizedBox(
+                  height: 20,
+                )
+              ],
+            ),
+          )
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline), label: 'Create Build'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined), label: 'Settings'),
+        ],
+        onTap: (value) {},
       ),
     );
   }
